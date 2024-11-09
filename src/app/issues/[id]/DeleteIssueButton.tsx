d0cb4f67d3971e661b410aaa9383f6f216f77b2f
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-11-09 20:13:43 +0430

    handle error when delete issue

--- src/app/issues/[id]/DeleteIssueButton.tsx
+++ src/app/issues/[id]/DeleteIssueButton.tsx
@@ -10,25 +10,29 @@ interface Props {
     issueId: number;
 }
 
-const DeleteIssueButton = ({issueId}:Props) => {
+const DeleteIssueButton = ({issueId}: Props) => {
     const [open, setOpen] = useState(false);
     const router = useRouter();
+    const [error, setError] = useState({hasError: false, message: ""});
 
-    const handleDeleteIssue = async (issueId:number) => {
+    const handleDeleteIssue = async (issueId: number) => {
         try {
             await axios.delete(`http://localhost:3000/api/issues/${issueId}`);
             setOpen(false);
             router.push("/issues");
             router.refresh();
-        }catch (e) {
+        } catch (e) {
+            const message = e instanceof Error ? e.message : "Failed to delete issue";
             console.log(e);
+            setOpen(false)
+            setError({hasError: true, message});
         }
     }
 
     return (
         <>
             <button
-                className="flex gap-x-2 items-center border border-red-400 rounded-md px-4 py-1 text-red-600 hover:text-red-700 dark:text-red-400 dark:hover:text-red-500"
+                className="flex gap-x-2 items-center justify-center bg-red-500 dark:bg-transparent dark:border dark:border-red-400 rounded-md px-4 py-2 text-white dark:text-red-400 dark:hover:text-red-500"
                 onClick={() => setOpen(true)}><TrashIcon className="w-4 h-4"/> Delete Issue
             </button>
             <Dialog open={open} onClose={setOpen} className="relative z-10">
@@ -49,7 +53,8 @@ const DeleteIssueButton = ({issueId}:Props) => {
                                         <ExclamationTriangleIcon aria-hidden="true" className="h-6 w-6 text-red-600"/>
                                     </div>
                                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                        <DialogTitle as="h3" className="text-base font-semibold text-gray-900 dark:text-gray-200">
+                                        <DialogTitle as="h3"
+                                                     className="text-base font-semibold text-gray-900 dark:text-gray-200">
                                             Delete Issue
                                         </DialogTitle>
                                         <div className="mt-2">
@@ -85,6 +90,53 @@ const DeleteIssueButton = ({issueId}:Props) => {
                     </div>
                 </div>
             </Dialog>
+
+
+            <Dialog open={error.hasError} onClose={() => setError({hasError: false, message: ""})}
+                    className="relative z-10">
+                <DialogBackdrop
+                    transition
+                    className="fixed inset-0 bg-gray-500 bg-opacity-65 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+                />
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <DialogPanel
+                            transition
+                            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                        >
+                            <div className="bg-white dark:bg-gray-800 dark:text-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                <div className="sm:flex sm:items-start">
+                                    <div
+                                        className="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-red-100 dark:bg-transparent dark:border dark:border-red-600 sm:mx-0 sm:h-10 sm:w-10">
+                                        <ExclamationTriangleIcon aria-hidden="true" className="h-6 w-6 text-red-600"/>
+                                    </div>
+                                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                        <DialogTitle as="h3"
+                                                     className="text-base font-semibold text-red-500 dark:text-gray-200">
+                                            Error
+                                        </DialogTitle>
+                                        <div className="mt-2">
+                                            <p className="text-sm text-gray-700 dark:text-gray-400">
+                                                {error.message}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bg-gray-50 dark:bg-gray-800 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                <button
+                                    type="button"
+                                    onClick={() => setError(prev => ({...prev, hasError: false}))}
+                                    className="inline-flex w-full justify-center rounded-md bg-red-600 dark:bg-transparent dark:border dark:border-red-500 dark:hover:border-red-600 dark:text-red-500 dark:hover:text-red-600 transition-colors px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                >
+                                    Ok
+                                </button>
+                            </div>
+                        </DialogPanel>
+                    </div>
+                </div>
+            </Dialog>
+
         </>
 
     );
--- src/app/issues/[id]/EditIssueButton.tsx
+++ src/app/issues/[id]/EditIssueButton.tsx
@@ -5,7 +5,7 @@ import {EditIcon} from "@/lib/icons";
 const EditIssueButton = ({issueId}: { issueId: number }) => {
     return (
         <Link href={`/issues/${issueId}/edit`}
-              className="flex gap-x-2 items-center border border-indigo-400 rounded-md px-4 py-1 text-indigo-600 hover:text-indigo-700 dark:text-indigo-400 dark:hover:text-indigo-500 ">
+              className="flex gap-x-2 items-center justify-center bg-indigo-500 dark:bg-transparent dark:border dark:border-indigo-400 rounded-md px-4 py-2 text-white dark:text-indigo-400 dark:hover:text-indigo-500 ">
             <EditIcon className="w-4 h-4"/>
             Edit
         </Link>
