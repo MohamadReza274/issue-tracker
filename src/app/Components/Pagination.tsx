4b57943be5cedc8524c143f9067c2b29dad74966
MohamadReza <reza.mohamadi.98115@gmail.com>
2024-11-24 12:10:18 +0430

    implement pagination fo r test

--- src/app/Components/Pagination.tsx
+++ src/app/Components/Pagination.tsx
@@ -1,9 +1,11 @@
+"use client";
 import {
   ChevronDoubleLeftIcon,
   ChevronDoubleRightIcon,
   ChevronLeftIcon,
   ChevronRightIcon,
 } from "@heroicons/react/16/solid";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -14,10 +16,17 @@ interface Props {
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
   const pageCount = Math.ceil(itemCount / pageSize);
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-  if (pageCount <= 1) {
-    return null;
-  }
+  if (pageCount <= 1) return null;
+
+  const handlePageChange = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
   return (
     <nav
       aria-label="Pagination"
@@ -32,24 +41,28 @@ const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
       </div>
       <div className="flex flex-1 justify-between gap-x-2 sm:justify-end">
         <button
+          onClick={() => handlePageChange(1)}
           disabled={currentPage === 1}
           className="relative inline-flex disabled:cursor-not-allowed items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
         >
           <ChevronDoubleLeftIcon className="w-5 h-5" />
         </button>
         <button
+          onClick={() => handlePageChange(currentPage - 1)}
           disabled={currentPage === 1}
           className="relative inline-flex disabled:cursor-not-allowed items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
         >
           <ChevronLeftIcon className="w-5 h-5" />
         </button>
         <button
+          onClick={() => handlePageChange(currentPage + 1)}
           disabled={currentPage === pageCount}
           className="relative ml-3 disabled:cursor-not-allowed inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
         >
           <ChevronRightIcon className="w-5 h-5" />
         </button>
         <button
+          onClick={() => handlePageChange(pageCount)}
           disabled={currentPage === pageCount}
           className="relative disabled:cursor-not-allowed inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
         >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Pagination from "./Components/Pagination";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <div className=" font-[family-name:var(--font-geist-sans)]">
       <h1>Home page</h1>
-      <Pagination pageSize={10} itemCount={100} currentPage={1} />
+      <Pagination
+        pageSize={10}
+        itemCount={100}
+        currentPage={parseInt(searchParams.page) || 1}
+      />
     </div>
   );
 }
